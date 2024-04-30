88c95bfa4df1bd69604937464aac2ed16098e483
Liam Lawrence <liamrlawrence@gmail.com>
2024-04-30 19:05:21 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,109 @@
+# crayons.nvim
+crayons.nvim dynamically manages and applies color schemes and theme settings. It provides a straightforward way to switch between various themes and configurations directly within Neovim.
+
+## Features
+- **Theme Management**: Easily switch between multiple themes with predefined variants.
+- **Custom Keybindings**: Assign custom keybindings for different themes and variants.
+- **Transparency Support**: Option to enable or disable transparency for themes.
+- **Extensible**: Add more themes and configurations as needed.
+- **Persistent**: Settings are saved between sessions.
+
+## Requirements
+- Neovim 0.5+
+- [cabinet.nvim](https://github.com/liamrlawrence/cabinet.nvim) for configuration management.
+- The themes that you will be using.
+
+## Installation
+Use your preferred package manager to install crayons:
+
+[lazy.nvim](https://github.com/folke/lazy.nvim)
+```lua
+return {
+    "liamrlawrence/crayons.nvim",
+    dependencies = {
+        "liamrlawrence/cabinet.nvim",
+        -- Themes go here
+    },
+
+    config = function()
+        require("crayons").setup()
+    end
+}
+```
+
+
+## Configuration
+The `crayons.nvim` setup function allows you to customize and extend the plugin according to your needs. Below are the configuration options available:
+
+### Adding New Themes
+You can add new themes or modify existing ones by including them in the `themes` table during setup. Each theme has several variants (standard, light, dark, darkest). Here’s how you can add a new theme:
+
+```lua
+require("crayons").setup({
+    themes = {
+        {   -- Theme #1
+            name = "gruvbox",
+            variants = {
+                standard = "gruvbox-medium",
+                light    = "gruvbox-light",
+                dark     = "gruvbox-dark",
+                darkest  = "gruvbox-dark",
+            }
+        },
+        {   -- Theme #2
+            name = "tokyonight",
+            variants = {
+                standard = "tokyonight-storm",
+                light    = "tokyonight-day",
+                dark     = "tokyonight-night",
+                darkest  = "tokyonight-night",
+            }
+        },
+        -- ...
+    }
+})
+```
+
+### Adding Special Themes
+Special themes are ones that have a single variant and you want to assign unique settings, such as transparency and a specific keybindings. You can configure special themes in the `special_themes` section of the setup:
+
+```lua
+require("crayons").setup({
+    special_themes = {
+        {
+            name = "vscode",             -- Theme name
+            mode = "dark",               -- "dark" or "light"
+            transparency = true,         -- true or false
+            keybinding = "<leader>TTv",  -- custom binding
+        },
+        -- ...
+    }
+})
+```
+
+### Customizing Keybindings
+Keybindings allow you to quickly switch between different theme variants. You can customize these keybindings in the `keybindings` field of the setup. Here's how to set up custom keybindings for switching themes:
+
+```lua
+require("crayons").setup({
+    keybindings = {
+        standard = "<leader>TT",
+        light    = "<leader>TL",
+        dark     = "<leader>TD",
+        darkest  = "<leader>Td"
+    }
+})
+```
+
+## Usage
+### Switching Themes
+You can use crayons.nvim to switch between themes and their variants through pre-configured keybindings:
+
+-   **Standard Theme**: `<leader>TT#` - Switch to the standard variant of a theme, where `#` is the index number of the theme.
+-   **Light Theme**: `<leader>TL#` - Switch to the light variant of a theme.
+-   **Dark Theme**: `<leader>TD#` - Switch to the dark variant of a theme.
+-   **Darkest Theme with Transparency**: `<leader>Td#` - Switch to the darkest variant of a theme with transparency enabled.
+
+## License
+crayons.nvim is released under the MIT License.
+
---
+++ lua/crayons/init.lua
@@ -0,0 +1,118 @@
+local Config = require("cabinet").config_manager
+
+local M = {}
+
+
+
+M.crayon_config = {
+    themes = {
+        {   -- 1
+            name = "built-in",
+            variants = {
+                standard = "default",
+                light    = "shine",
+                dark     = "desert",
+                darkest  = "default",
+            }
+        },
+        -- Add more themes as needed
+    },
+
+    keybindings = {
+        standard = "<leader>TT",
+        light    = "<leader>TL",
+        dark     = "<leader>TD",
+        darkest  = "<leader>Td"
+    },
+
+    special_themes = {
+        -- Example: {
+            -- name = "vscode",
+            -- mode = "dark",
+            -- transparency = false,
+            -- keybinding = "<leader>TTv"
+        -- },
+    }
+}
+
+
+local function set_theme(theme_name, theme_mode, theme_transparency)
+    vim.o.background = theme_mode
+    vim.cmd.colorscheme(theme_name)
+    if theme_transparency then
+        vim.api.nvim_set_hl(0, "LineNr",            { bg = "none" })
+        vim.api.nvim_set_hl(0, "SignColumn",        { bg = "none" })
+        vim.api.nvim_set_hl(0, "Normal",            { bg = "none" })
+        vim.api.nvim_set_hl(0, "NormalFloat",       { bg = "none" })
+        vim.api.nvim_set_hl(0, "GitGutterAdd",      { bg = "none", fg = "#009900" })
+        vim.api.nvim_set_hl(0, "GitGutterChange",   { bg = "none", fg = "#bbbb00" })
+        vim.api.nvim_set_hl(0, "GitGutterDelete",   { bg = "none", fg = "#ff2222" })
+        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    end
+
+    -- Highlight current parameter when looking at a Signature
+    local marked = vim.api.nvim_get_hl(0, { name = "PMenu" })
+    vim.api.nvim_set_hl(0, "LspSignatureActiveParameter", {
+        fg = marked.fg,
+        bg = marked.bg,
+        ctermfg = marked.ctermfg,
+        ctermbg = marked.ctermbg,
+        bold = true
+    })
+
+    -- Save settings
+    local config_data = {
+        theme_name = theme_name,
+        theme_mode = theme_mode,
+        theme_transparency = theme_transparency
+    }
+    Config.save("theme_config", config_data)
+end
+
+
+function M.setup(user_config)
+    -- Setup Cabinet to store theme settings
+    Config.setup({
+        config_path = "crayons"
+    })
+
+    -- User config replaces default
+    if user_config then
+        M.crayon_config = vim.tbl_deep_extend("force", M.crayon_config, user_config)
+    end
+
+    -- Register keybindings for standard themes
+    for index, theme_info in ipairs(M.crayon_config.themes) do
+        local name = theme_info.name
+        local themes = theme_info.variants
+        vim.keymap.set("n", M.crayon_config.keybindings.standard .. index, function() set_theme(themes.standard, "dark", false) end)
+        vim.keymap.set("n", M.crayon_config.keybindings.light .. index, function() set_theme(themes.light, "light", false) end)
+        vim.keymap.set("n", M.crayon_config.keybindings.dark .. index, function() set_theme(themes.dark, "dark", false) end)
+        vim.keymap.set("n", M.crayon_config.keybindings.darkest .. index, function() set_theme(themes.darkest, "dark", true) end)
+    end
+
+    -- Register keybindings for special themes
+    for _, special in ipairs(M.crayon_config.special_themes) do
+        vim.keymap.set("n", special.keybinding, function() set_theme(special.name, special.mode, special.transparency) end)
+    end
+
+    -- Load saved configuration
+    local theme_name, theme_mode, theme_transparency
+    local loaded_config = Config.load("theme_config")
+    if loaded_config then
+        theme_name = loaded_config.theme_name
+        theme_mode = loaded_config.theme_mode
+        theme_transparency = loaded_config.theme_transparency
+    else
+        -- Fallback if config file doesn't exist
+        theme_name = M.crayon_config.themes[1]["variants"]["standard"]
+        theme_mode = "dark"
+        theme_transparency = false
+    end
+    set_theme(theme_name, theme_mode, theme_transparency)
+end
+
+
+
+return M
+
